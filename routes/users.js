8fe2937378ab1f21e8d71ea09412517e1cc7e6f1
Lior Shalev <liorshalev01@gmail.com>
2015-11-09 11:21:45 +0200

    updates

--- routes/users.js
+++ routes/users.js
@@ -14,16 +14,24 @@ router.param('id', function (req, res, next, id) {
     next();
 });
 
-router.get('/user', function (req, res) {
-    res.json("hello");
+router.get('/users', function (req, res, next) {
+    User.find().exec(function (err, users) {
+        if (err) {
+            next(err);
+        }
+
+        res.json(users);
+    });
 });
 
-router.post('/user/register', function (req, res, next) {
+router.post('/users/register', function (req, res, next) {
     User.findOne({'facebookId': req.body.facebookId}).exec(function (err, user) {
         if (err) {
             next(err);
 
-        } else if (user === null) {
+        }
+
+        if (user === null) {
             user = new User();
             user.name = req.body.name;
             user.email = null;
@@ -42,8 +50,25 @@ router.post('/user/register', function (req, res, next) {
     });
 });
 
-router.get('/user/:id', function (req, res) {
+router.get('/users/:id', function (req, res) {
     res.json("hello " + req.params.id);
+});
+
+router.get('/users/:id/asks', function (req, res, next) {
+    var ask = new Ask();
+    ask.userId = req.params.id;
+    ask.createDate = Date.now();
+    ask.status = req.body.status;
+    ask.content = req.body.content;
+    ask.isAnonymous = req.body.isAnonymous;
+
+    ask.save(function (err) {
+        if (err) {
+            next(err);
+        }
+    });
+
+    res.json(ask);
 });
 
 router.post('/user/:id/asks', function (req, res, next) {
